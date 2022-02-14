4fd2db4f4b911c4253a2f827f2f3ee54e432d19c
Andkow <x00152236@mytudublin.ie>
2022-02-14 10:47:36 +0000

    Backend basic set up, getting products from backend

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Computer Desk with Bookshelf",
+    image: "/images/ComputerDesk.jpg",
+    description:
+      "Computer Desk with Bookshelf, Study Computer Laptop Table with 4 Tier DIY Storage Shelves Writing Table for Home Office",
+    brand: "No Name",
+    category: "Desks",
+    price: 109.99,
+    countInStock: 9,
+    rating: 4.5,
+    numReviews: 19,
+  },
+  {
+    _id: "2",
+    name: "Modern L Shaped Corner Desk",
+    image: "/images/CornerDesk.jpg",
+    description:
+      "Modern L shape designed computer desk make full use of your space. Easy to fit in a corner to maximize s limited space",
+    brand: "No Name",
+    category: "Desks",
+    price: 199.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 27,
+  },
+  {
+    _id: "3",
+    name: "Sideboard Chest of Drawers Massa in Antique Steel",
+    image: "/images/ChestOfDrawers.jpg",
+    description:
+      "The sideboard Massa is the ideal storage solution for your living room. It is equipped with two doors and four drawers.",
+    brand: "N/A",
+    category: "Living",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Industrial Bookshelf, Metal and Wood Bookcase",
+    image: "/images/industrialBookshelf.jpg",
+    description:
+      "Industrial Bookshelf, Metal and Wood Bookcase, Tall 4 Tier Book Shelves for Living Room, Wooden Office Shelving Unit (Dark Gray Oak)",
+    brand: "N/A",
+    category: "Living",
+    price: 99.99,
+    countInStock: 7,
+    rating: 5,
+    numReviews: 27,
+  },
+  {
+    _id: "5",
+    name: "Shoe Shelf Mesh Storage Shelf",
+    image: "/images/ShoeBench.jpg",
+    description:
+      "Shoe Bench With a rustic brown surface and industrial-style black frame, this bench brings a touch of delicacy to your interior.",
+    brand: "LIVE",
+    category: "Industriale",
+    price: 79.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Coffee Table with Open Shelf",
+    image: "/images/coffeeTable.jpg",
+    description:
+      "JOISCOPE Coffee Table with Open Shelf for Spacious Storage,2-Tier Coffee Desk,Industrial-Style Living Room Desk(Vintage Oak Finish)",
+    brand: "Amazon",
+    category: "Industrial-Style",
+    price: 199.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
