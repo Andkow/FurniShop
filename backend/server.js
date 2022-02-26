ceea34277be97d1d7506902f84a4fa1757b6c93d
Andkow <x00152236@mytudublin.ie>
2022-02-26 10:43:42 +0000

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./conf/mongoDB.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
